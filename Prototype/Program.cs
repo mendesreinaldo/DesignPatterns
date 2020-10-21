52e6acf8fcf973d1554cdc8a219c2010813db8e3
Reinaldo de Souza Mendes <reinaldo.mendes@adm.unip.br>
2020-10-21 09:28:11 -0300

    PROTOTYPE

--- Prototype/Program.cs
+++ Prototype/Program.cs
@@ -10,6 +10,14 @@ namespace Prototype
     {
         static void Main(string[] args)
         {
+            GerenciadorNuvens gerenciadorNuvens = new GerenciadorNuvens();
+            gerenciadorNuvens["padrão"] = new NuvemConcreta("branca", "Azul");
+            gerenciadorNuvens["personalizada"] = new NuvemConcreta("branco", "laranja");
+
+            NuvemConcreta um = gerenciadorNuvens["padrão"].Clone() as NuvemConcreta;
+            NuvemConcreta dois = gerenciadorNuvens["padrão"].Clone() as NuvemConcreta;
+            NuvemConcreta tres = gerenciadorNuvens["personalizada"].Clone() as NuvemConcreta;
+            Console.ReadKey();
         }
     }
 }
